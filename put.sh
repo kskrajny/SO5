fefb604206cd7e4039c51afcaaaef56689c6e38b
kskrajny <j.skrajny@student.uw.edu.pl>
2020-05-27 13:49:46 +0200

    juhu

---
+++ put.sh
@@ -0,0 +1,7 @@
+sshpass -p "root" scp -P 10023 read.c root@localhost:/usr/src/minix/fs/mfs/read.c
+sshpass -p "root" scp -P 10023 open.c root@localhost:/usr/src/minix/fs/mfs/open.c
+sshpass -p "root" scp -P 10023 glo.h root@localhost:/usr/src/minix/fs/mfs/glo.h
+sshpass -p "root" scp -P 10023 link.c root@localhost:/usr/src/minix/fs/mfs/link.c
+sshpass -p "root" scp -P 10023 main.c root@localhost:/usr/src/minix/fs/mfs/main.c
+
+sshpass -p "root" scp -P 10023 ini root@localhost:/ini
